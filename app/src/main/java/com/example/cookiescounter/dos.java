24e05bebb7eeb4fb77a4ebee70f93896ce5d8906
zartos <daikerumaru@hotmail.com>
2019-04-09 10:05:04 -0500

    Se configuro el boton wallpaper para que tome la imagen seleccionada y la ponga como fondo de pantalla del telefono, se agrego permiso en xml de modificar el wallpaper

--- app/src/main/java/com/example/cookiescounter/dos.java
+++ app/src/main/java/com/example/cookiescounter/dos.java
@@ -1,15 +1,20 @@
 package com.example.cookiescounter;
 
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.IOException;
+
 public class dos extends AppCompatActivity implements View.OnClickListener {
 
-   int movil;
-   ImageView seleccion;
+    int movil;
+    ImageView seleccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +40,7 @@ public class dos extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()){
+        switch (v.getId()) {
 
             case R.id.r1:
                 seleccion.setImageResource(R.drawable.r1);
@@ -58,9 +63,19 @@ public class dos extends AppCompatActivity implements View.OnClickListener {
                 movil = R.drawable.r5;
                 break;
             case R.id.wallpaper:
-
+                Bitmap fondo = BitmapFactory.decodeStream(getResources().openRawResource(movil));
+                WallpaperManager myWallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+                try {
+                    myWallpaperManager.setBitmap(fondo);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
                 break;
 
+
         }
+
     }
 }
