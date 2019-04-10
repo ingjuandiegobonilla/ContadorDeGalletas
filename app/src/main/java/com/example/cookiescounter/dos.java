0413c4b0ea8a5333bd1475cd02b80f2aec5061de
zartos <daikerumaru@hotmail.com>
2019-04-10 10:57:05 -0500

    Se agrego una actividad con un webview en ventana completa, se activaron propiedades de javascript y zoom, se configuro para que URL's subsecuentes abran en el mismo webview y se agregaron permisos en el manifest.xml para acceso a internet

--- app/src/main/java/com/example/cookiescounter/dos.java
+++ app/src/main/java/com/example/cookiescounter/dos.java
@@ -2,6 +2,8 @@ package com.example.cookiescounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class dos extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class dos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dos);
+
+        WebView miNavegador = findViewById(R.id.navegador);  //Enlazamos variable con elemento xml
+        miNavegador.getSettings().setJavaScriptEnabled(true); //Habilitamos uso de JavaScript
+        miNavegador.getSettings().setBuiltInZoomControls(true); //Habilitamos propiedad de zoom
+
+        miNavegador.loadUrl("https://animeflv.net/"); //Introducimos la url que deseamos cargar
+
+        miNavegador.setWebViewClient(new WebViewClient() {  //Se establece como cliente webview para abrir las proximas url sobre el mismo vizor
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
     }
 }
